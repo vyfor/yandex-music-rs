ff9a345826ace01d8ec479883e2d64ee7570e387
ReBlast <reblastoff@gmail.com>
2024-06-19 15:20:26 +0500

    feat: implement `/account`

---
+++ src/api/account/get_account_settings.rs
@@ -0,0 +1,24 @@
+use crate::{
+    api::{RequestPath, Response},
+    model::settings::AccountSettings,
+    YandexMusicClient,
+};
+
+pub struct AccountSettingsRequest {}
+
+impl RequestPath for AccountSettingsRequest {
+    fn path(&self) -> String {
+        String::from("account/settings")
+    }
+}
+
+impl YandexMusicClient {
+    pub async fn get_account_settings(
+        &self,
+    ) -> Result<AccountSettings, crate::ClientError> {
+        let response: Response =
+            self.get(&AccountSettingsRequest {}.path()).await?;
+
+        Ok(serde_json::from_value::<AccountSettings>(response.result)?)
+    }
+}
---
+++ src/model/account_model/settings.rs
@@ -0,0 +1,27 @@
+use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountSettings {
+    pub uid: i32,
+    pub last_fm_scrobbling_enabled: bool,
+    pub shuffle_enabled: bool,
+    pub volume_percents: i32,
+    pub modified: String,
+    pub facebook_scrobbling_enabled: bool,
+    pub add_new_track_on_playlist_top: bool,
+    pub user_music_visibility: String,
+    pub user_social_visibility: String,
+    pub rbt_disabled: bool,
+    pub theme: String,
+    pub promos_disabled: bool,
+    pub auto_play_radio: bool,
+    pub sync_queue_enabled: bool,
+    pub ads_disabled: Option<bool>,
+    pub disk_enabled: Option<bool>,
+    pub show_disk_tracks_in_library: Option<bool>,
+    pub explicit_forbidden: Option<bool>,
+    pub child_mod_enabled: Option<bool>,
+    pub wizard_is_passed: Option<bool>,
+    pub user_collection_hue: Option<i32>,
+}
