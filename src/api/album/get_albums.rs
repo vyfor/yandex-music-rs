45690b2f50514a5e9f194873c3ad8e6c8e5e9b30
ReBlast <reblastoff@gmail.com>
2024-06-21 13:06:02 +0500

    feat: implement POST `/albums`

---
+++ src/api/album/get_albums.rs
@@ -0,0 +1,35 @@
+use crate::{
+    api::{RequestPath, Response},
+    model::album::Album,
+    YandexMusicClient,
+};
+
+pub struct AlbumsRequest {}
+
+impl RequestPath for AlbumsRequest {
+    fn path(&self) -> String {
+        String::from("albums")
+    }
+}
+
+impl YandexMusicClient {
+    pub async fn get_albums(
+        &self,
+        album_ids: &[i32],
+    ) -> Result<Vec<Album>, crate::ClientError> {
+        let response: Response = self
+            .post_with_form(
+                &AlbumsRequest {}.path(),
+                vec![(
+                    "album-ids",
+                    &album_ids
+                        .iter()
+                        .map(|&id| id.to_string() + ",")
+                        .collect::<String>(),
+                )],
+            )
+            .await?;
+
+        Ok(serde_json::from_value::<Vec<Album>>(response.result)?)
+    }
+}
--- src/api/album/mod.rs
+++ src/api/album/mod.rs
@@ -1,2 +1,3 @@
 pub mod get_album;
 pub mod get_album_with_tracks;
+pub mod get_albums;
